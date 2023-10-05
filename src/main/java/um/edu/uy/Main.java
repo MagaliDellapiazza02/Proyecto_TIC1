9c7b51335504646d6eec531346e7a9821c63f617
MagaliDellapiazza02 <mdellapiazza@correo.um.edu.uy>
2023-10-05 10:13:26 -0300

    Wrapper agregado a gitignore.

--- src/main/java/um/edu/uy/Main.java
+++ src/main/java/um/edu/uy/Main.java
@@ -5,6 +5,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import um.edu.uy.ui.JavaFXApplication;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 
 @SpringBootApplication
 public class Main {
