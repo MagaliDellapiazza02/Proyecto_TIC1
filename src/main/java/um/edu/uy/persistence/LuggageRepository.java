dd5948653a9bcccabdaa57a1f2631b7faf406b5b
balta <bhenderson@correo.um.edu.uy>
2023-11-19 23:12:44 -0300

    @Component en LuggageRepository

--- src/main/java/um/edu/uy/persistence/LuggageRepository.java
+++ src/main/java/um/edu/uy/persistence/LuggageRepository.java
@@ -1,6 +1,7 @@
 package um.edu.uy.persistence;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Component;
 import um.edu.uy.business.entities.Airport;
 import um.edu.uy.business.entities.Flight;
 import um.edu.uy.business.entities.Luggage;
@@ -8,6 +9,8 @@ import um.edu.uy.business.entities.Passenger;
 
 import java.util.List;
 
+import java.util.List;
+@Component
 public interface LuggageRepository extends CrudRepository<Luggage, String> {
 
     Luggage findByTrackingCode (String trackingCode);
@@ -17,6 +20,8 @@ public interface LuggageRepository extends CrudRepository<Luggage, String> {
     List<Luggage> findAllByFlight(Flight flight);
 
     List<Luggage> findAllByPassenger(Passenger passenger);
+
+    List<Luggage> findByPassengerId(long id);
 
 }
 
